a08b82b459ed1b69e584f57ae6db683053c82704
Jere Laine <jere.lainee02@gmail.com>
2022-12-10 20:35:53 +0200

    Fetch data from REST API and return a list

--- src/Notes.jsx
+++ src/Notes.jsx
@@ -1,4 +1,28 @@
+import { useEffect, useState } from "react";
+
+const Note = ({ data }) => {
+  return (
+    <>
+      <li>
+        {data.course.name} (id {data.course.id}) {data.timestamp}
+      </li>
+      <span>{data.text}</span>
+    </>
+  );
+};
+
 const Notes = () => {
+  const url = "https://luentomuistiinpano-api.deta.dev/notes/";
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((response) => {
+        setData(response);
+      });
+  }, []);
+
   return (
     <>
       <h2>Saved notes</h2>
@@ -7,6 +31,11 @@ const Notes = () => {
         <option>all</option>
         <option>Ohjelmointi 1</option>
       </select>
+      <ul>
+        {data.map((r, i) => (
+          <Note key={i} data={r}></Note>
+        ))}
+      </ul>
     </>
   );
 };
